5d98f6aacf903a10e6cf8e455ba32ffbc05cd086
Анастасия <anastasia@MacBook-Air-Anastasia.local>
2017-11-11 10:24:31 +0300

    add remove border_amount% of the gene at the beginning and at the end in search_file_by_codon.py

--- scripts/search_file_by_codon.py
+++ scripts/search_file_by_codon.py
@@ -1,7 +1,12 @@
-import os
+import os, math
 
-direct = "/Users/anastasia/PycharmProjects/course_work/testData/"
-out_file = "/Users/anastasia/PycharmProjects/course_work/files_with_stop_codon.txt"
+# direct = "/Users/anastasia/PycharmProjects/course_work/data/fileTest/test"
+# out_file = "/Users/anastasia/PycharmProjects/course_work/output/files_with_stop_codon.txt"
+
+direct = "/mnt/lustre/potapova/200_flies/all_genes/"
+out_file = "/mnt/lustre/nknyazeva/courseWork4/repository/output/files_with_stop_codonscd .txt"
+
+border_amount = 0.05
 
 for folders, subfolders, files in os.walk(direct):
     file_list = files
@@ -14,8 +19,16 @@ for file in file_list:
     for line in f1:
         if line[0] != ">":
             str = line[:-4]
-            for codon in range(0, len(str), 3):
-                current_codon = str[codon:codon + 3]
+            #remove border_amount% of the gene at the beginning and at the end
+            removal_length = int(math.floor(len(str) * border_amount))
+            remainder_of_division = removal_length % 3
+            if remainder_of_division == 1:
+                removal_length = removal_length - 1
+            if remainder_of_division == 2:
+                removal_length = removal_length - 2
+            str_wo_border = str[removal_length: - removal_length]
+            for codon in range(0, len(str_wo_border), 3):
+                current_codon = str_wo_border[codon:codon + 3]
                 if current_codon == 'TAA' or current_codon == 'TAG' or current_codon == 'TGA':
                     f2.write(file + "\n")
                     tmp = False
