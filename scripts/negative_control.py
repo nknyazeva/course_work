401c698d38af19ead77adafb55c69eb088cf38f1
Анастасия <anastasia@MacBook-Air-Anastasia.local>
2017-11-11 16:13:20 +0300

    path

--- scripts/negative_control.py
+++ scripts/negative_control.py
@@ -5,18 +5,18 @@ a = sys.argv
 a = a[1:]
 threshold = str(a[0])
 
-in_file = '_'.join(["/Users/anastasia/PycharmProjects/course_work/output/threshold_genes_with_stop", str(threshold)])
-in_file_wo_stop = "/Users/anastasia/PycharmProjects/course_work/output/files_wo_stop_codons.txt"
-in_file_no_threshold = '_'.join(["/Users/anastasia/PycharmProjects/course_work/output/no_threshold_genes_with_stop", str(threshold)])
-out_file_frequency_codon = '_'.join(["/Users/anastasia/PycharmProjects/course_work/output/ng_description_pairs_genes", str(threshold)])
-out_file_frequency_codon_counting = '_'.join(["/Users/anastasia/PycharmProjects/course_work/output/ng_frequency_pairs_genes", str(threshold)])
+# in_file = '_'.join(["/Users/anastasia/PycharmProjects/course_work/output/threshold_genes_with_stop", str(threshold)])
+# in_file_wo_stop = "/Users/anastasia/PycharmProjects/course_work/output/files_wo_stop_codons.txt"
+# in_file_no_threshold = '_'.join(["/Users/anastasia/PycharmProjects/course_work/output/no_threshold_genes_with_stop", str(threshold)])
+# out_file_frequency_codon = '_'.join(["/Users/anastasia/PycharmProjects/course_work/output/ng_description_pairs_genes", str(threshold)])
+# out_file_frequency_codon_counting = '_'.join(["/Users/anastasia/PycharmProjects/course_work/output/ng_frequency_pairs_genes", str(threshold)])
 
 
-# in_file = '_'.join(["/mnt/lustre/nknyazeva/courseWork4/repository/output/threshold_genes_with_stop", str(threshold)])
-# in_file_wo_stop = "/mnt/lustre/nknyazeva/courseWork4/repository/output/files_wo_stop_codons.txt"
-# in_file_no_threshold = '_'.join(["/mnt/lustre/nknyazeva/courseWork4/repository/output/no_threshold_genes_with_stop", str(threshold)])
-# out_file_frequency_codon = '_'.join(["/mnt/lustre/nknyazeva/courseWork4/repository/output/description_pairs_genes", str(threshold)])
-# out_file_frequency_codon_counting = '_'.join(["/mnt/lustre/nknyazeva/courseWork4/repository/output/frequency_pairs_genes", str(threshold)])
+in_file = '_'.join(["/mnt/lustre/nknyazeva/courseWork4/repository/output/threshold_genes_with_stop", str(threshold)])
+in_file_wo_stop = "/mnt/lustre/nknyazeva/courseWork4/repository/output/files_wo_stop_codons.txt"
+in_file_no_threshold = '_'.join(["/mnt/lustre/nknyazeva/courseWork4/repository/output/no_threshold_genes_with_stop", str(threshold)])
+out_file_frequency_codon = '_'.join(["/mnt/lustre/nknyazeva/courseWork4/repository/output/ng_description_pairs_genes", str(threshold)])
+out_file_frequency_codon_counting = '_'.join(["/mnt/lustre/nknyazeva/courseWork4/repository/output/ng_frequency_pairs_genes", str(threshold)])
 
 
 number_individual = float(197)
