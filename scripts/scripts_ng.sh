d2b40d8ce7190a87fd5adea8b93dea0a29f1dd4f
Анастасия <anastasia@MacBook-Air-Anastasia.local>
2017-11-12 17:42:54 +0300

    add more pairs genes in negative_control_more_pairs.py (all pairs)

--- scripts/scripts_ng.sh
+++ scripts/scripts_ng.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #PBS -l walltime=100:00:00
 #PBS -d.
-python negative_control.py 0.1
-python negative_control.py 0.2
-python negative_control.py 0.3
-python negative_control.py 0.4
-python negative_control.py 0.5
+python negative_control_more_pairs.py 0.1
+python negative_control_more_pairs.py 0.2
+python negative_control_more_pairs.py 0.3
+python negative_control_more_pairs.py 0.4
+python negative_control_more_pairs.py 0.5
